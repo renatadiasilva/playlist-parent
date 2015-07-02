2284706df5de35c1f3870eef976305f0ef715c19
renatadiasilva <renatadiasilva@gmail.com>
2015-07-02 20:10:39 +0100

    warn log Encrypt (tirei)

--- playlist-ejb/src/main/java/dei/uc/pt/aor/EncryptPass.java
+++ playlist-ejb/src/main/java/dei/uc/pt/aor/EncryptPass.java
@@ -8,16 +8,10 @@ import javax.ejb.Stateless;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 @Stateless
 public class EncryptPass {
 	
-	private static final Logger log = LoggerFactory.getLogger(EncryptPass.class);
-
     public String encrypt(String password) {
-    	log.info("Encrypting password");
         String securedPassword = "";
         if (null == password) {
             return null;
@@ -34,7 +28,6 @@ public class EncryptPass {
             
         } catch (NoSuchAlgorithmException e) {
         	String errorMsg = "Error while encrypting"+ "password: "+e.getMessage();
-        	log.error(errorMsg);
 			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(errorMsg));
 			return null;
         }        
