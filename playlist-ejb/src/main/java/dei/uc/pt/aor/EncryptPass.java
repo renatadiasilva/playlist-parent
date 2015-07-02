e8651f8bfdf4f7c1ed8848cc0987d04db7127718
renatadiasilva <renatadiasilva@gmail.com>
2015-07-02 21:51:42 +0100

    Revert "warn log Encrypt (tirei)"
    This reverts commit 2284706df5de35c1f3870eef976305f0ef715c19.

--- playlist-ejb/src/main/java/dei/uc/pt/aor/EncryptPass.java
+++ playlist-ejb/src/main/java/dei/uc/pt/aor/EncryptPass.java
@@ -8,10 +8,16 @@ import javax.ejb.Stateless;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Stateless
 public class EncryptPass {
 	
+	private static final Logger log = LoggerFactory.getLogger(EncryptPass.class);
+
     public String encrypt(String password) {
+    	log.info("Encrypting password");
         String securedPassword = "";
         if (null == password) {
             return null;
@@ -28,6 +34,7 @@ public class EncryptPass {
             
         } catch (NoSuchAlgorithmException e) {
         	String errorMsg = "Error while encrypting"+ "password: "+e.getMessage();
+        	log.error(errorMsg);
 			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(errorMsg));
 			return null;
         }        
